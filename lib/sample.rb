7f2fb7f2b2799991044b215876a1d424d4bf8f87
saisa6153 <yuki.tannai@gmail.com>
2013-05-20 08:04:21 +0900

    initial commit

--- lib/sample.rb
+++ lib/sample.rb
@@ -1,7 +1,29 @@
 # -*- encoding: utf-8 -*-
 
-class Sample
-  def say
-    "Hello TDD BootCamp!"
+class Version
+  def initialize(var)
+    @var = var
+  end
+
+  def self.valid?(var)
+    !!(var =~ /^JDK\d*u\d*$/)
+  end
+
+  def self.parse(var)
+    return raise unless self.valid?(var)
+    Version.new(var)
+  end
+
+  def family_number
+    return $1.to_i if /JDK(\d*)/ =~ @var
+  end
+
+  def update_number
+    return $1.to_i if /JDK\d*u(\d*)/ =~ @var
+  end
+
+  def lt(ver_obj)
+    return update_number < ver_obj.update_number if family_number == ver_obj.family_number
+    family_number < ver_obj.family_number
   end
 end
--- spec/lib/sample_spec.rb
+++ spec/lib/sample_spec.rb
@@ -1,13 +1,72 @@
 # -*- encoding: utf-8 -*-
 require 'spec_helper'
 
-describe Sample do
+describe Version do
+  it "Versionが#family_numberと#update_numberメソッドを実行できる" do
+    Version.new("").should respond_to("family_number", "update_number")
+  end
 
-  describe "#say" do
-    it "should return 'Hello TDD BootCamp!'" do
-      sut = Sample.new
-      sut.say.should == "Hello TDD BootCamp!"
+  describe ".valid?" do
+    it "JDK7u40を渡したらTrueを返す" do
+      Version.valid?("JDK7u40").should be_true
+    end
+
+    it "正規のバージョン以外の文字を渡したらFalseを返す" do
+      Version.valid?("hoge").should be_false
+      Version.valid?("fuga").should be_false
+    end
+
+    it "JDK7u9xを渡したらFalseを返す" do
+      Version.valid?("JDK7u9x").should be_false
+    end
+
+  end
+
+  describe ".parse" do
+    it "family_numberとupdate_numberメソッドを実行できるオブジェクトを返す" do
+      Version.parse("JDK7u40").should respond_to("family_number", "update_number")
+    end
+
+    it "JDK7u4xを渡したら例外を返すこと" do
+      proc { Version.parse("JDK7u4x")}.should raise_error
     end
   end
 
+  describe "#family_number" do
+    it "ファミリーナンバーを返すこと" do
+      Version.new("JDK7u40").family_number.should == 7
+      Version.new("JDK8u40").family_number.should == 8
+    end
+  end
+
+  describe "#update_number" do
+    it "アップデートナンバーを返すこと" do
+      Version.new("JDK7u40").update_number.should == 40
+      Version.new("JDK7u35").update_number.should == 35
+    end
+  end
+
+  describe "#lt" do
+    it "自分より小さいバージョンを渡したときはtrueを返すこと" do
+      u40 = Version.new("JDK7u40")
+      u51 = Version.new("JDK7u51")
+      u40.lt(u51).should be_true
+    end
+
+    it "自分より小さいバージョンを渡したときはtrueを返すこと" do
+      f7 = Version.new("JDK7u40")
+      f8 = Version.new("JDK8u00")
+      f7.lt(f8).should be_true
+    end
+  end
+ 
+  describe "#lern" do
+    it "JDK(数字)を渡したらその数字を返す" do
+      def test(var)
+        return $1.to_i if /JDK(\d*)/ =~ var
+        return nil
+      end
+      test("JDK7").should == 7
+    end
+  end
 end
